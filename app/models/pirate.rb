77f2f99f3c45b00d272917a563ba3147ac735e08
quadroturkey <reesemb1@live.com>
2020-02-20 11:53:33 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates.clear
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ship = []
+
+  def initialize
+    @@ship << self
+  end
+
+  def self.all
+    @@ship
+  end
+
+  def self.clear
+    @@ship.clear
+  end
+
 end
